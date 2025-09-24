efb2ed14fd4f3bf29903254a70545b86f9546252
deekshithgowda85 <152056807+deekshithgowda85@users.noreply.github.com>
2025-09-24 10:53:28 +0530

    Initial Deployment

--- automl/src/components/theme-toggle.tsx
+++ automl/src/components/theme-toggle.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react'
 import { Moon, Sun } from 'lucide-react'
-import { useTheme } from '../../../../DEMOML/src/app/components/providers/theme-provider'
+import { useTheme } from '../app/components/providers/theme-provider'
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme()
